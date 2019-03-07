52612288f2cc7dc1f8ebcd9a04fa0879ff318d1a
FrankT <306933@gmail.com>
2019-03-07 14:16:10 -0800

    Initial commit

---
+++ src/Pack/CoffeeShoppeGUI.java
@@ -0,0 +1,93 @@
+package Pack;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class CoffeeShoppeGUI extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Jim's Coffee Shoppe");
+		
+		Button btn = new Button();
+		Label label1 = new Label();
+		TextField textField = new TextField ();
+		HBox hb = new HBox();
+		
+		btn.setText("Test button 1");
+		btn.setOnAction(new EventHandler<ActionEvent> () {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("This is the action of the button");
+			}
+			
+		});
+		
+		hb.getChildren().addAll(label1, textField);
+		hb.setSpacing(10);
+		
+		
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
