2c24513b3f9ac6e4cb9aa04d0c48c57f113c4d37
DaffodilPlate67 <vincerosane@outlook.com>
2019-03-11 16:19:15 -0700

    Cleanup Cleanup Cleanup
    I got tired of the code that made it harder to read so yeah

--- src/Pack/CoffeeShoppeGUI.java
+++ src/Pack/CoffeeShoppeGUI.java
@@ -25,6 +25,8 @@ public class CoffeeShoppeGUI extends Application {
 
     //Declaring a new decimal format cause it's annoying me
     static DecimalFormat df = new DecimalFormat("#.##");
+    //List root getter
+    ObservableList<javafx.scene.Node> list;
     //Counting the amount of orders
     static private int totSmall = 0;
     static private int totMed = 0;
@@ -35,11 +37,13 @@ public class CoffeeShoppeGUI extends Application {
     static private int rawSugarC = 0;
     static private int espressoShotC = 0;
     static private String computeName = "";
-    private Text text;
-    private TextField tf;
+    static private Text text;
+    static private TextField tf;
     //Do texts for the order numbers
-    private Text smallT, medT, bigT;
-    private Text creamT, sugarT, espressoT;
+    static private Text smallT, medT, bigT;
+    static private Text creamT, sugarT, espressoT;
+    //Button declaration
+    static private Button b, btn, btn2, btn3, cream, rawS, shot, compute, sB;
     // Initializing values for the buttons
     private double small = 1.25;
     private double med = 1.75;
@@ -47,8 +51,6 @@ public class CoffeeShoppeGUI extends Application {
     private double eP = 0.75;
 
     public static void main(String[] args) {
-    	// Making my scanner for the 
-    	
         launch(args);
     }
 
@@ -108,10 +110,100 @@ public class CoffeeShoppeGUI extends Application {
         computeAlert.showAndWait();
     }
 
+    private static void formatbtn(){
+        smallT.setX(270);
+        smallT.setY(230);
+        smallT.setFont(new Font(20));
+        smallT.setVisible(false);
+        btn.setMinSize(156, 50);
+        btn.setText("Small Coffee: $1.25");
+        btn.setLayoutX(100);
+        btn.setLayoutY(200);
+    }
+
+    private static void formatbtn2(){
+        medT.setX(smallT.getX());
+        medT.setY(281);
+        medT.setFont(new Font(20));
+        medT.setVisible(false);
+        btn2.setMinSize(156, 50);
+        btn2.setText("Medium Coffee: $1.75");
+        btn2.setLayoutX(btn.getLayoutX());
+        btn2.setLayoutY(250);
+    }
+
+    private static void formatbtn3(){
+        bigT.setX(medT.getX());
+        bigT.setY(330);
+        bigT.setFont(new Font(20));
+        bigT.setVisible(false);
+        btn3.setMinSize(156, 50);
+        btn3.setText("Large Coffee: $2.30");
+        btn3.setLayoutX(btn2.getLayoutX());
+        btn3.setLayoutY(300);
+    }
+
+    private static void formatCream(){
+        creamT.setFont(new Font(20));
+        creamT.setX(580);
+        creamT.setY(smallT.getY());
+        creamT.setVisible(false);
+        cream.setMinSize(156, 50);
+        cream.setText("Cream: Free");
+        cream.setLayoutX(400);
+        cream.setLayoutY(200);
+    }
+
+    private static void formatSugar(){
+        sugarT.setFont(new Font(20));
+        sugarT.setX(creamT.getX());
+        sugarT.setY(medT.getY());
+        sugarT.setVisible(false);
+        rawS.setMinSize(156, 50);
+        rawS.setText("Raw sugar: Free");
+        rawS.setLayoutX(cream.getLayoutX());
+        rawS.setLayoutY(250);
+    }
+
+    private static void formatEspresso(){
+        espressoT.setFont(new Font(20));
+        espressoT.setX(creamT.getX());
+        espressoT.setY(bigT.getY());
+        shot.setMinSize(156, 50);
+        shot.setText("Extra shots: $0.75");
+        shot.setLayoutX(cream.getLayoutX());
+        shot.setLayoutY(300);
+    }
+
+    private static void formatCompute(){
+        compute.setText("COMPUTE");
+        compute.setLayoutX(350);
+        compute.setLayoutY(400);
+    }
+
+    private static void showScene(Stage primaryStage, Group root){
+        Scene scene = new Scene(root, 800, 600);
+
+        // Adding scene to the stage
+        primaryStage.setScene(scene);
+
+        // Displaying the contents of the stage
+        primaryStage.show();
+    }
+
+    private static void formatSB(){
+        sB.setMinSize(150, 150);
+        sB.setLayoutX(550);
+        sB.setLayoutY(400);
+    }
+
     @Override
     public void start(Stage primaryStage) {
 
         primaryStage.setTitle("Jim Coffee Shoppe");
+
+        // Creating a Group object
+        Group root = new Group();
 
         // Button for name change
         Button b = new Button("Want to change your name?");
@@ -146,20 +238,10 @@ public class CoffeeShoppeGUI extends Application {
         text.setX(275);
         text.setY(50);
 
-        // Creating a Group object
-        Group root = new Group();
-
         // Creating small coffee button options
-        Button btn = new Button();
+        btn = new Button();
         smallT = new Text();
-        smallT.setX(230);
-        smallT.setY(230);
-        smallT.setFont(new Font(20));
-        smallT.setVisible(false);
-        btn.setMinSize(156, 50);
-        btn.setText("Small Coffee: $1.25");
-        btn.setLayoutX(50);
-        btn.setLayoutY(200);
+        formatbtn();
         btn.setOnAction(event -> {
             totSmall++;
             totSum += small;
@@ -168,16 +250,9 @@ public class CoffeeShoppeGUI extends Application {
             smallT.setVisible(true);
         });
         // Creating medium size coffee buttons
-        Button btn2 = new Button();
+        btn2 = new Button();
         medT = new Text();
-        medT.setX(230);
-        medT.setY(281);
-        medT.setFont(new Font(20));
-        medT.setVisible(false);
-        btn2.setMinSize(156, 50);
-        btn2.setText("Medium Coffee: $1.75");
-        btn2.setLayoutX(50);
-        btn2.setLayoutY(250);
+        formatbtn2();
         btn2.setOnAction(event -> {
             totMed++;
             totSum += med;
@@ -186,16 +261,9 @@ public class CoffeeShoppeGUI extends Application {
             medT.setVisible(true);
         });
         // Creating large size coffee buttons
-        Button btn3 = new Button();
+        btn3 = new Button();
         bigT = new Text();
-        bigT.setX(230);
-        bigT.setY(330);
-        bigT.setFont(new Font(20));
-        bigT.setVisible(false);
-        btn3.setMinSize(156, 50);
-        btn3.setText("Large Coffee: $2.30");
-        btn3.setLayoutX(50);
-        btn3.setLayoutY(300);
+        formatbtn3();
         btn3.setOnAction(event -> {
             totLarge++;
             totSum += large;
@@ -205,16 +273,9 @@ public class CoffeeShoppeGUI extends Application {
         });
 
         // Selection for cream button
-        Button cream = new Button();
+        cream = new Button();
         creamT = new Text();
-        creamT.setFont(new Font(20));
-        creamT.setX(525);
-        creamT.setY(smallT.getY());
-        creamT.setVisible(false);
-        cream.setMinSize(156, 50);
-        cream.setText("Cream: Free");
-        cream.setLayoutX(346);
-        cream.setLayoutY(200);
+        formatCream();
         cream.setOnAction(event -> {
             creamC++;
             System.out.println("# of cream: " + creamC);
@@ -222,16 +283,9 @@ public class CoffeeShoppeGUI extends Application {
             creamT.setVisible(true);
         });
         // Selection for rawSugar button
-        Button rawS = new Button();
+        rawS = new Button();
         sugarT = new Text();
-        sugarT.setFont(new Font(20));
-        sugarT.setX(creamT.getX());
-        sugarT.setY(medT.getY());
-        sugarT.setVisible(false);
-        rawS.setMinSize(156, 50);
-        rawS.setText("Raw sugar: Free");
-        rawS.setLayoutX(346);
-        rawS.setLayoutY(250);
+        formatSugar();
         rawS.setOnAction(event -> {
             rawSugarC++;
             System.out.println("# of rawSugar: " + rawSugarC);
@@ -239,15 +293,9 @@ public class CoffeeShoppeGUI extends Application {
             sugarT.setVisible(true);
         });
         // Selection for shot button
-        Button shot = new Button();
+        shot = new Button();
         espressoT = new Text();
-        espressoT.setFont(new Font(20));
-        espressoT.setX(creamT.getX());
-        espressoT.setY(bigT.getY());
-        shot.setMinSize(156, 50);
-        shot.setText("Extra shots: $0.75");
-        shot.setLayoutX(346);
-        shot.setLayoutY(300);
+        formatEspresso();
         shot.setOnAction(event -> {
             espressoShotC++;
             totSum += eP;
@@ -257,30 +305,26 @@ public class CoffeeShoppeGUI extends Application {
         });
 
         // Selection for compute button
-        Button compute = new Button();
+        compute = new Button();
         compute.setMinSize(150, 150);
-        compute.setText("COMPUTE");
-        compute.setLayoutX(350);
-        compute.setLayoutY(400);
+        formatCompute();
         compute.setOnAction(event -> {
             System.out.println("total sum: " + totSum);
             showComputeDialog();
         });
 
         //Button to close the entire program
-        Button sB = new Button("Click here to close");
-        sB.setMinSize(150, 150);
-        sB.setLayoutX(550);
-        sB.setLayoutY(400);
+        sB = new Button("Click here to close");
+        formatSB();
         sB.setOnAction(event -> Platform.exit());
+
         // Retrieving the observable list object
-        ObservableList<javafx.scene.Node> list = root.getChildren();
+        list = root.getChildren();
 
         // Setting the text object as a node to the group object
         list.add(text);
         list.add(tf);
         list.add(b);
-
         list.add(btn);
         list.add(btn2);
         list.add(btn3);
@@ -289,7 +333,6 @@ public class CoffeeShoppeGUI extends Application {
         list.add(shot);
         list.add(compute);
         list.add(sB);
-
         list.add(smallT);
         list.add(medT);
         list.add(bigT);
@@ -298,12 +341,6 @@ public class CoffeeShoppeGUI extends Application {
         list.add(sugarT);
 
         // Creating a scene object
-        Scene scene = new Scene(root, 800, 600);
-
-        // Adding scene to the stage
-        primaryStage.setScene(scene);
-
-        // Displaying the contents of the stage
-        primaryStage.show();
+        showScene(primaryStage, root);
     }
 }
